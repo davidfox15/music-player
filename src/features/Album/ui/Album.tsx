d5431e0cad13bdcdfde80b28bdad71bc876d10ee
Давид Лисицын <davidfox15@yandex.ru>
2024-08-31 01:30:11 +0300

    Add animation play/pause for Album

--- src/features/Album/ui/Album.tsx
+++ src/features/Album/ui/Album.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import classNames from 'classnames'
 import styles from './styles.module.css'
 import Image from 'next/image'
+import { useEffect, useRef, useState } from 'react'
 
 interface IAlbum {
     isPlay: boolean
@@ -8,11 +11,40 @@ interface IAlbum {
     title: string
 }
 
+const rotateAnimation = {
+    keyframes: [{ transform: 'rotate(0)' }, { transform: 'rotate(360deg)' }],
+    options: {
+        duration: 4000,
+        iterations: Infinity,
+        easing: 'linear',
+    },
+}
+
 export default function Album({ isPlay, image, title }: IAlbum) {
+    const animationRef = useRef<Animation | null>(null)
+    const albumRef = useRef<HTMLDivElement | null>(null)
+
+    useEffect(() => {
+        if (albumRef.current) {
+            animationRef.current = albumRef.current.animate(
+                rotateAnimation.keyframes,
+                rotateAnimation.options
+            )
+        }
+    }, [albumRef])
+
+    useEffect(() => {
+        if (animationRef.current) {
+            if (isPlay) {
+                animationRef.current.play()
+            } else {
+                animationRef.current.pause()
+            }
+        }
+    }, [isPlay, animationRef])
+
     return (
-        <div
-            className={classNames(styles.album, { [styles.rotating]: isPlay })}
-        >
+        <div ref={albumRef} className={classNames(styles.album)}>
             <Image src={image} alt={title} fill={true} />
         </div>
     )
