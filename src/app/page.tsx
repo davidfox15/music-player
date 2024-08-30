63a92fc33dda133694a3c3fc8ea167f821d6a9cc
Давид Лисицын <davidfox15@yandex.ru>
2024-08-31 00:40:55 +0300

    Create start page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import styles from './app.module.css'
 import bg from '../../public/background.png'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Home() {
     return (
@@ -8,7 +9,9 @@ export default function Home() {
             className={styles.main}
             style={{ backgroundImage: `url(${bg.src})` }}
         >
-            <Link href="/player">Go to Player</Link>
+            <Link href="/player">
+                <Image src="play.svg" alt="play" width={150} height={150} />
+            </Link>
         </main>
     )
 }
